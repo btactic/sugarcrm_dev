ea8029a4a057c29c3be943a28c59fc2ef92193f0
Sergi Almacellas Abellana <sergi.almacellas@btactic.com>
2013-04-30 11:37:43 +0200

    Fix error on reports_to where that prevents where filter to work properly.

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -5166,11 +5166,11 @@ class SugarBean
     {
         if(isset($this->field_defs['assigned_user_id']))
         {
-            return " $this->table_name.assigned_user_id ='$user_id' or $this->table_name.assigned_user_id in (select id from users where reports_to_id = '$user_id') ";
+            return " ( $this->table_name.assigned_user_id ='$user_id' or $this->table_name.assigned_user_id in (select id from users where reports_to_id = '$user_id') ) ";
         }
         if(isset($this->field_defs['created_by']))
         {
-            return " $this->table_name.created_by ='$user_id' or $this->table_name.created_by in (select id from users where reports_to_id = '$user_id') ";
+            return " ( $this->table_name.created_by ='$user_id' or $this->table_name.created_by in (select id from users where reports_to_id = '$user_id') ) ";
         }
         return '';
     }
